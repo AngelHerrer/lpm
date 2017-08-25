82c0b44cf5ffabdd763ef78bc2ef5bf5b2c244e3
Angel Herrera <enyel26435@live.com.mx>
2017-08-25 16:37:56 -0500

    lpm url

---
+++ conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+ $servidor = "localhost";
+ $usuario = "root";
+ $password = "root";
+
+if (!$liga = mysqli_connect($servidor, $usuario, $password)) {
+    echo "No se encuentra el Servidor";
+} else {
+
+    if (!mysqli_select_db($liga, "lpmprueba")) {
+
+        echo "No se encuentra la Base de datos";
+    } else {
+
+        //echo 'conectado';
+    }
+}
+?>
+
