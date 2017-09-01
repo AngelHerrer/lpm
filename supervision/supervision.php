b9f3cbbaee460f3343b37d8969d87706f5074312
Angel Herrera <enyel26435@live.com.mx>
2017-09-01 18:24:16 -0500

    GPS

--- supervision/supervision.php
+++ supervision/supervision.php
@@ -24,6 +24,47 @@ $supervisores = new Supervisores();
     </head>
     <body>
         <div class="container">
+               <script type="text/javascript">
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(mostrarPosicion, mostrarErrores, opciones);	
+		} else {
+			alert("Tu navegador no soporta la geolocalización, actualiza tu navegador.");
+		}
+
+		function mostrarPosicion(posicion) {
+			var latitud = posicion.coords.latitude;
+			var longitud = posicion.coords.longitude;
+			var precision = posicion.coords.accuracy;
+			var fecha = new Date(posicion.timestamp);
+			$('#posicion').append("<p>Latitud: " + latitud + "</p>");
+			$('#posicion').append("<p>Longitud:" + longitud + "</p>");
+			$('#posicion').append("<p>Precisión: " + precision + " metros </p>"); 
+			$('#posicion').append("<p>Fecha: " + fecha + "</p>");  
+		}
+
+		function mostrarErrores(error) {
+			switch (error.code) {
+		 		case error.PERMISSION_DENIED:
+		  			alert('Permiso denegado por el usuario'); 
+		  			break;
+		   		case error.POSITION_UNAVAILABLE:
+		    		alert('Posición no disponible');
+		     		break; 
+		     	case error.TIMEOUT:
+		      		alert('Tiempo de espera agotado');
+		       		break;
+		        default:
+		         	alert('Error de Geolocalización desconocido :' + error.code);
+			}
+		}
+
+		var opciones = {
+			enableHighAccuracy: true,
+			timeout: 10000,
+			maximumAge: 1000
+		};
+	</script>
+        <div id="posicion"></div>
             <div class="row">
                 <?php
                 if ($_GET['succes'] == "true") {
