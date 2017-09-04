6edc18978cdff8bf9e497ac3fd562cb3acc750b4
Angel Herrera <enyel26435@live.com.mx>
2017-09-04 14:42:14 -0500

    Resumen

--- supervision/conexion.php
+++ supervision/conexion.php
@@ -1,14 +1,14 @@
 <?php
 
 $servidor = "localhost";
-$usuario = "root";
-$password = "root";
+$usuario = "rootA";
+$password = "password!1!";
 
 if (!$liga = mysqli_connect($servidor, $usuario, $password)) {
     echo "No se encuentra el Servidor";
 } else {
 
-    if (!mysqli_select_db($liga, "LPM")) {
+    if (!mysqli_select_db($liga, "goispcom_supervisor")) {
 
         echo "No se encuentra la Base de datos";
     } else {
